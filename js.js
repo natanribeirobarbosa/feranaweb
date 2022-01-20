0e04365985cf7623c98dff6871e3d450b5625607
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2022-01-20 07:04:07 -0300

    Header #3

--- js.js
+++ js.js
@@ -6,11 +6,11 @@
     if (c1==0) {
        c1=c1+1
        
-        document.querySelector('.header2').style.display="block"
+        document.querySelector('.lista1').style.display="flex"
         
     } else {
         c1=0
-        document.querySelector('.header2').style.display="none"
+        document.querySelector('.lista1').style.display="none"
        
         
     }
