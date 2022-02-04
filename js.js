60da7109f11f5bddba9795514f3a792ce6ceff38
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2022-02-04 17:20:19 -0300

    artigo1

--- js.js
+++ js.js
@@ -3,18 +3,7 @@ window.document.getElementById('lista0').style.transform="translateY(-150%)"
 
 
 
-/*
 
-function hoveremnegrito(a) {
-    console.log('entrou!')
-    document.getElementById(a).style.fontWeight="bold"
-
-    
-}
-function hoversemnegrito(b) {
-    document.getElementById(b).style.fontWeight="normal"
-}            
-             */
                
 
 function lista0animacao(a) {
@@ -52,52 +41,26 @@ function lista1() {
     if (window.document.getElementById('lista1').style.display=="none") {
         console.log('lista1 abriu')
         window.document.getElementById('lista1').style.display="flex"
-        window.document.getElementById('i1').style.fontWeight="600"
+        
     }
     else{
         console.log('lista1 fechou')
         window.document.getElementById('lista1').style.display="none"
         window.document.getElementById('lista2-1').style.display="none"
-        window.document.getElementById('i1').style.fontWeight="normal"
-        window.document.getElementById('i21').style.fontWeight="normal"
+        
         
     }
  
 }
 
-/*function lista12() {
-    if (c2==0) {
-        window.document.getElementById('lista1-2').style.display="flex"
-        window.document.getElementById('item2').style.opacity="80%"
-        c2++
-    } else {
-        window.document.getElementById('lista1-2').style.display="none"
-        window.document.getElementById('item2').style.opacity="100%"
-        c2--
-    }
-}
-
-function lista13() {
-    if (c3==0) {
-        window.document.getElementById('lista1-3').style.display="flex"
-        window.document.getElementById('item3').style.opacity="80%"
-        c3++
-    } else {
-        window.document.getElementById('lista1-3').style.display="none"
-        window.document.getElementById('item3').style.opacity="100%"
-        c3--
-    }
-}
-*/
 function lista21() {
     if (window.document.getElementById('lista2-1').style.display=="none") {
         window.document.getElementById('lista2-1').style.display="flex"
-        window.document.getElementById('i21').style.fontWeight="600"
+        
     }
     else{
         window.document.getElementById('lista2-1').style.display="none"
-        window.document.getElementById('i21').style.fontWeight="normal"
-        window.document.getElementById('i21').style.fontWeight="normal"
+     
     }
 }
 
