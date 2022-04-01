fa46afc232cfaba8f0d6528063480ecf3301b3dd
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2022-03-31 22:43:10 -0300

    31/03/22

--- js.js
+++ js.js
@@ -1,43 +1,41 @@
 
-/*INDICES E TÍTULOS*/
-
+ /*Logo do whatsaap para colocar nos títulos*/
 var whatsaap=`<span class="_30yMe"><svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39"><path fill="rgb(12 12 12)" d="M10.7 32.8l.6.3c2.5 1.5 5.3 2.2 8.1 2.2 8.8 0 16-7.2 16-16 0-4.2-1.7-8.3-4.7-11.3s-7-4.7-11.3-4.7c-8.8 0-16 7.2-15.9 16.1 0 3 .9 5.9 2.4 8.4l.4.6-1.6 5.9 6-1.5z"></path><path fill="rgb(255,255,255)" d="M32.4 6.4C29 2.9 24.3 1 19.5 1 9.3 1 1.1 9.3 1.2 19.4c0 3.2.9 6.3 2.4 9.1L1 38l9.7-2.5c2.7 1.5 5.7 2.2 8.7 2.2 10.1 0 18.3-8.3 18.3-18.4 0-4.9-1.9-9.5-5.3-12.9zM19.5 34.6c-2.7 0-5.4-.7-7.7-2.1l-.6-.3-5.8 1.5L6.9 28l-.4-.6c-4.4-7.1-2.3-16.5 4.9-20.9s16.5-2.3 20.9 4.9 2.3 16.5-4.9 20.9c-2.3 1.5-5.1 2.3-7.9 2.3zm8.8-11.1l-1.1-.5s-1.6-.7-2.6-1.2c-.1 0-.2-.1-.3-.1-.3 0-.5.1-.7.2 0 0-.1.1-1.5 1.7-.1.2-.3.3-.5.3h-.1c-.1 0-.3-.1-.4-.2l-.5-.2c-1.1-.5-2.1-1.1-2.9-1.9-.2-.2-.5-.4-.7-.6-.7-.7-1.4-1.5-1.9-2.4l-.1-.2c-.1-.1-.1-.2-.2-.4 0-.2 0-.4.1-.5 0 0 .4-.5.7-.8.2-.2.3-.5.5-.7.2-.3.3-.7.2-1-.1-.5-1.3-3.2-1.6-3.8-.2-.3-.4-.4-.7-.5h-1.1c-.2 0-.4.1-.6.1l-.1.1c-.2.1-.4.3-.6.4-.2.2-.3.4-.5.6-.7.9-1.1 2-1.1 3.1 0 .8.2 1.6.5 2.3l.1.3c.9 1.9 2.1 3.6 3.7 5.1l.4.4c.3.3.6.5.8.8 2.1 1.8 4.5 3.1 7.2 3.8.3.1.7.1 1 .2h1c.5 0 1.1-.2 1.5-.4.3-.2.5-.2.7-.4l.2-.2c.2-.2.4-.3.6-.5s.4-.4.5-.6c.2-.4.3-.9.4-1.4v-.7s-.1-.1-.3-.2z"></path></svg></span>`
 
-if (!!document.querySelector('article.artigos')) {
-    
 
-var item1_1=window.document.getElementById('i1').innerText
-var item2_1=window.document.getElementById('i21').innerText
-var item3_1=window.document.getElementById('i3_1_1').innerText
-var item3_2=window.document.getElementById('i3_1_2').innerText
+if (!!document.querySelector('article.artigos'))/*teste para saber se a página é uma pagina de artigos*/{
+
+    /*criação dos nomes de todos os primeiros itens*/
+    itensprimarios=[]
+var item1=window.document.getElementById('i1').innerText
+
+var item11=window.document.getElementById('i11').innerText
+var item111=window.document.getElementById('i111').innerText
+var item112=window.document.getElementById('i112').innerText
 var numerodoartigo = window.document.querySelector('body').className
 
 
 
 
 
+
 switch (numerodoartigo) {
-    case 'i311':
-
-    window.document.getElementById('pg1').innerHTML=item1_1
-    window.document.getElementById('pg2').innerHTML=item2_1
-    window.document.getElementById('pg3').innerHTML=item3_1
-   
-
-        break;
-    
-    case 'i312':
- 
-    window.document.querySelector('h1.titulo').innerHTML=item3_2+whatsaap
-    window.document.getElementById('pg1').innerHTML=item1_1
-    window.document.getElementById('pg2').innerHTML=item2_1
-    window.document.getElementById('pg3').innerHTML=item3_2
-   
- 
-    
+    case 'i111':
+    window.document.getElementById('pg1').innerHTML=item1
+    window.document.getElementById('pg2').innerHTML=item11
+    window.document.getElementById('pg3').innerHTML=item111
+    var numerodoartigo2=1
     break;
+    
+    case 'i112':
+    window.document.querySelector('h1.titulo').innerHTML=item3_2+whatsaap
+    window.document.getElementById('pg1').innerHTML=item1
+    window.document.getElementById('pg2').innerHTML=item1
+    window.document.getElementById('pg3').innerHTML=item1
+    break;
+
     default:
-        break;
+    break;
 }
 
 var titulos= new Object();
@@ -122,47 +120,52 @@ var titulos= new Object();
     for (let index = 0; index <= whatsaap.nome.length; index++) {
         var numerodecelulas=index;
     }
-    var r1=parseInt(numerodecelulas*0.25)
-    var r2=parseInt(numerodecelulas*0.50)
-    var r3=parseInt(numerodecelulas*0.75)
-    var r4=parseInt(numerodecelulas*1.00)
-
-    console.log(numerodecelulas)
-    console.log(r1)
-    console.log(r2)
-    console.log(r3)
-    console.log(r4)
-    console.log(whatsaap)
-
-    const recomendacao=[getRandom(0,r1),getRandom(r1,r2),getRandom(r2,r3),getRandom(r3,r4)]
-
-    function getRandom(min,max) {
-        var n1=Math.random() * (max-min)+min;
-         return parseInt(n1)
-      }
-     console.log(recomendacao);
+  
+    switch (numerodoartigo2) {
+        case 1:
+    document.querySelector(`div#artigos-c a:nth-of-type(1) h1`).innerHTML=whatsaap.nome[numerodoartigo2+1]
+    document.querySelector(`div#artigos-c a:nth-of-type(1) h1`).href=whatsaap.link[numerodoartigo2+1]
     
+    document.querySelector(`div#artigos-c a:nth-of-type(2) h1`).innerHTML=whatsaap.nome[numerodoartigo2+2]
+    document.querySelector(`div#artigos-c a:nth-of-type(2) h1`).href=whatsaap.link[numerodoartigo2+2]
+
+    document.querySelector(`div#artigos-c a:nth-of-type(3) h1`).innerHTML=whatsaap.nome[numerodoartigo2+3]
+    document.querySelector(`div#artigos-c a:nth-of-type(3) h1`).href=whatsaap.link[numerodoartigo2+3]
+
+    document.querySelector(`div#artigos-c a:nth-of-type(4) h1`).innerHTML=whatsaap.nome[numerodoartigo2+4]
+    document.querySelector(`div#artigos-c a:nth-of-type(4) h1`).href=whatsaap.link[numerodoartigo2+4]
+            break;
+    
+        default:
+            break;
+    }
+  
+
+    
+
+  
+
+    /*função para criar numeros aleatórios e usar nos artigos relacionados*//*
+function getRandom(min,max){
+    var n1=Math.random() * (max-min)+min;
+        return parseInt(n1) }
+     
+    document.querySelector(`div#artigos-c a:nth-of-type(1) h1`).innerHTML=whatsaap.nome[recomendacao[2]]
+    document.querySelector(`div#artigos-c a:nth-of-type(1) h1`).href=whatsaap.link[recomendacao[2]]
+    
+    document.querySelector(`div#artigos-c a:nth-of-type(2) h1`).innerHTML=whatsaap.nome[recomendacao[1]]
+    document.querySelector(`div#artigos-c a:nth-of-type(2) h1`).href=whatsaap.link[recomendacao[1]]
+
+    document.querySelector(`div#artigos-c a:nth-of-type(3) h1`).innerHTML=whatsaap.nome[recomendacao[0]]
+    document.querySelector(`div#artigos-c a:nth-of-type(3) h1`).href=whatsaap.link[recomendacao[0]]
+
+    document.querySelector(`div#artigos-c a:nth-of-type(4) h1`).innerHTML=whatsaap.nome[recomendacao[3]]
+    document.querySelector(`div#artigos-c a:nth-of-type(4) h1`).href=whatsaap.link[recomendacao[3]]
 
  
 
-
-/*
-    document.querySelector(`div#recomendados a:nth-of-type(1) article h1`).innerHTML=whatsaap.nome[recomendacao[2]]
-    document.querySelector(`div#recomendados a:nth-of-type(1)`).href=whatsaap.link[recomendacao[2]]
-    
-    document.querySelector(`div#recomendados a:nth-of-type(2) article h1`).innerHTML=whatsaap.nome[recomendacao[1]]
-    document.querySelector(`div#recomendados a:nth-of-type(2)`).href=whatsaap.link[recomendacao[1]]
-
-    document.querySelector(`div#recomendados a:nth-of-type(3) article h1`).innerHTML=whatsaap.nome[recomendacao[0]]
-    document.querySelector(`div#recomendados a:nth-of-type(3)`).href=whatsaap.link[recomendacao[0]]
-
-    document.querySelector(`div#recomendados a:nth-of-type(4) article h1`).innerHTML=whatsaap.nome[recomendacao[3]]
-    document.querySelector(`div#recomendados a:nth-of-type(4)`).href=whatsaap.link[recomendacao[3]]
-
-   */
-    
 }
-
+*/
 
 
 /*contadores*/
@@ -261,4 +264,4 @@ function primeirospassos1(){
 
     }
    
-}
+}}
