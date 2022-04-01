4342a970937050a335dad1aa3e3d8d2e6ac4a79e
Natã Ribeiro Barbosa <95133483+natanribeirobarbosa@users.noreply.github.com>
2022-04-01 16:45:51 -0300

    01/04/22

--- js.js
+++ js.js
@@ -1,14 +1,73 @@
+window.document.getElementById('lista0').style.display="none"
+window.document.getElementById('lista1').style.display="none"
+window.document.getElementById('lista11').style.display="none"
 
- /*Logo do whatsaap para colocar nos títulos*/
-var whatsaap=`<span class="_30yMe"><svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39"><path fill="rgb(12 12 12)" d="M10.7 32.8l.6.3c2.5 1.5 5.3 2.2 8.1 2.2 8.8 0 16-7.2 16-16 0-4.2-1.7-8.3-4.7-11.3s-7-4.7-11.3-4.7c-8.8 0-16 7.2-15.9 16.1 0 3 .9 5.9 2.4 8.4l.4.6-1.6 5.9 6-1.5z"></path><path fill="rgb(255,255,255)" d="M32.4 6.4C29 2.9 24.3 1 19.5 1 9.3 1 1.1 9.3 1.2 19.4c0 3.2.9 6.3 2.4 9.1L1 38l9.7-2.5c2.7 1.5 5.7 2.2 8.7 2.2 10.1 0 18.3-8.3 18.3-18.4 0-4.9-1.9-9.5-5.3-12.9zM19.5 34.6c-2.7 0-5.4-.7-7.7-2.1l-.6-.3-5.8 1.5L6.9 28l-.4-.6c-4.4-7.1-2.3-16.5 4.9-20.9s16.5-2.3 20.9 4.9 2.3 16.5-4.9 20.9c-2.3 1.5-5.1 2.3-7.9 2.3zm8.8-11.1l-1.1-.5s-1.6-.7-2.6-1.2c-.1 0-.2-.1-.3-.1-.3 0-.5.1-.7.2 0 0-.1.1-1.5 1.7-.1.2-.3.3-.5.3h-.1c-.1 0-.3-.1-.4-.2l-.5-.2c-1.1-.5-2.1-1.1-2.9-1.9-.2-.2-.5-.4-.7-.6-.7-.7-1.4-1.5-1.9-2.4l-.1-.2c-.1-.1-.1-.2-.2-.4 0-.2 0-.4.1-.5 0 0 .4-.5.7-.8.2-.2.3-.5.5-.7.2-.3.3-.7.2-1-.1-.5-1.3-3.2-1.6-3.8-.2-.3-.4-.4-.7-.5h-1.1c-.2 0-.4.1-.6.1l-.1.1c-.2.1-.4.3-.6.4-.2.2-.3.4-.5.6-.7.9-1.1 2-1.1 3.1 0 .8.2 1.6.5 2.3l.1.3c.9 1.9 2.1 3.6 3.7 5.1l.4.4c.3.3.6.5.8.8 2.1 1.8 4.5 3.1 7.2 3.8.3.1.7.1 1 .2h1c.5 0 1.1-.2 1.5-.4.3-.2.5-.2.7-.4l.2-.2c.2-.2.4-.3.6-.5s.4-.4.5-.6c.2-.4.3-.9.4-1.4v-.7s-.1-.1-.3-.2z"></path></svg></span>`
+function lista0animacao() {
+ 
+    console.log('lista 0')
+    if (window.document.getElementById('lista0').style.display=="none") 
+    {
+        console.log('abriu!')
+        /*window.document.getElementById('contlist').style.width="unset"*/
+        window.document.getElementById('lista0').style.display="flex"
+        console.log('abriu!')
+        window.document.getElementById('l1').style.transform="rotate(45deg) translateY(10px) "
+        window.document.getElementById('l3').style.transform="rotate(-45deg) translateY(-10px)"
+        window.document.getElementById('l2').style.opacity="0%"
+    }
+    else{
+        console.log('fechou!')
+        /*window.document.getElementById('contlist').style.width="0"*/
+        window.document.getElementById('lista0').style.display="none"
+        window.document.getElementById('l1').style.transform="rotate(0deg) translateY(0px)"
+        window.document.getElementById('l3').style.transform="rotate(0deg) translateY(0px)"
+        window.document.getElementById('l2').style.opacity="100%"
+
+    }
+    
+}
+function lista1() {
+    if (window.document.getElementById('lista1').style.display=="none") {
+        console.log('lista1 abriu')
+        window.document.getElementById('lista1').style.display="flex"
+        window.document.getElementById('lista11').style.display="none"
+        
+    }
+    else{
+        console.log('lista1 fechou')
+        window.document.getElementById('lista1').style.display="none"
+        window.document.getElementById('lista11').style.display="none"
+        
+        
+    }
+ 
+}
+function lista11() {
+    if (window.document.getElementById('lista11').style.display=="none") {
+        console.log('referencias e ajuda')
+        window.document.getElementById('lista11').style.display="contents"
+       
+        
+    }
+    else{
+        console.log('lista1 fechou')
+        window.document.getElementById('lista11').style.display="none"
+      
+        
+        
+    }
+ 
+}
 
 
-if (!!document.querySelector('article.artigos'))/*teste para saber se a página é uma pagina de artigos*/{
 
-    /*criação dos nomes de todos os primeiros itens*/
-    itensprimarios=[]
+
+/*teste para saber se a página é uma pagina de artigos*/
+if (!!document.querySelector('article.artigos')){
+
+/*criação dos nomes de todos os primeiros itens*/
+    
 var item1=window.document.getElementById('i1').innerText
-
 var item11=window.document.getElementById('i11').innerText
 var item111=window.document.getElementById('i111').innerText
 var item112=window.document.getElementById('i112').innerText
@@ -92,25 +151,25 @@ var titulos= new Object();
 
     document.querySelector('#ajuda a').innerHTML="Ajuda e dicas"
 
-    let artigo1=document.querySelector('#lista2-1 li:nth-of-type(1) a').innerHTML
-    let artigo2=document.querySelector('#lista2-1 li:nth-of-type(2) a').innerHTML
-    let artigo3=document.querySelector('#lista2-1 li:nth-of-type(3) a').innerHTML
-    let artigo4=document.querySelector('#lista2-1 li:nth-of-type(4) a').innerHTML
-    let artigo5=document.querySelector('#lista2-1 li:nth-of-type(5) a').innerHTML
-    let artigo6=document.querySelector('#lista2-1 li:nth-of-type(6) a').innerHTML
-    let artigo7=document.querySelector('#lista2-1 li:nth-of-type(7) a').innerHTML
-    let artigo8=document.querySelector('#lista2-1 li:nth-of-type(8) a').innerHTML
-    let artigo9=document.querySelector('#lista2-1 li:nth-of-type(9) a').innerHTML
+    let artigo1=document.querySelector('#lista11 li:nth-of-type(1) a').innerHTML
+    let artigo2=document.querySelector('#lista11 li:nth-of-type(2) a').innerHTML
+    let artigo3=document.querySelector('#lista11 li:nth-of-type(3) a').innerHTML
+    let artigo4=document.querySelector('#lista11 li:nth-of-type(4) a').innerHTML
+    let artigo5=document.querySelector('#lista11 li:nth-of-type(5) a').innerHTML
+    let artigo6=document.querySelector('#lista11 li:nth-of-type(6) a').innerHTML
+    let artigo7=document.querySelector('#lista11 li:nth-of-type(7) a').innerHTML
+    let artigo8=document.querySelector('#lista11 li:nth-of-type(8) a').innerHTML
+    let artigo9=document.querySelector('#lista11 li:nth-of-type(9) a').innerHTML
 
-    let link1=document.querySelector('#lista2-1 li:nth-of-type(1) a').href
-    let link2=document.querySelector('#lista2-1 li:nth-of-type(2) a').href
-    let link3=document.querySelector('#lista2-1 li:nth-of-type(3) a').href
-    let link4=document.querySelector('#lista2-1 li:nth-of-type(4) a').href
-    let link5=document.querySelector('#lista2-1 li:nth-of-type(5) a').href
-    let link6=document.querySelector('#lista2-1 li:nth-of-type(6) a').href
-    let link7=document.querySelector('#lista2-1 li:nth-of-type(7) a').href
-    let link8=document.querySelector('#lista2-1 li:nth-of-type(8) a').href
-    let link9=document.querySelector('#lista2-1 li:nth-of-type(9) a').href
+    let link1=document.querySelector('#lista11 li:nth-of-type(1) a').href
+    let link2=document.querySelector('#lista11 li:nth-of-type(2) a').href
+    let link3=document.querySelector('#lista11 li:nth-of-type(3) a').href
+    let link4=document.querySelector('#lista11 li:nth-of-type(4) a').href
+    let link5=document.querySelector('#lista11 li:nth-of-type(5) a').href
+    let link6=document.querySelector('#lista11 li:nth-of-type(6) a').href
+    let link7=document.querySelector('#lista11 li:nth-of-type(7) a').href
+    let link8=document.querySelector('#lista11 li:nth-of-type(8) a').href
+    let link9=document.querySelector('#lista11 li:nth-of-type(9) a').href
 
     var whatsaap= {
         nome:[artigo1,artigo2,artigo3,artigo4,artigo5,artigo6,artigo7,artigo8,artigo9],
@@ -143,29 +202,7 @@ var titulos= new Object();
 
     
 
-  
 
-    /*função para criar numeros aleatórios e usar nos artigos relacionados*//*
-function getRandom(min,max){
-    var n1=Math.random() * (max-min)+min;
-        return parseInt(n1) }
-     
-    document.querySelector(`div#artigos-c a:nth-of-type(1) h1`).innerHTML=whatsaap.nome[recomendacao[2]]
-    document.querySelector(`div#artigos-c a:nth-of-type(1) h1`).href=whatsaap.link[recomendacao[2]]
-    
-    document.querySelector(`div#artigos-c a:nth-of-type(2) h1`).innerHTML=whatsaap.nome[recomendacao[1]]
-    document.querySelector(`div#artigos-c a:nth-of-type(2) h1`).href=whatsaap.link[recomendacao[1]]
-
-    document.querySelector(`div#artigos-c a:nth-of-type(3) h1`).innerHTML=whatsaap.nome[recomendacao[0]]
-    document.querySelector(`div#artigos-c a:nth-of-type(3) h1`).href=whatsaap.link[recomendacao[0]]
-
-    document.querySelector(`div#artigos-c a:nth-of-type(4) h1`).innerHTML=whatsaap.nome[recomendacao[3]]
-    document.querySelector(`div#artigos-c a:nth-of-type(4) h1`).href=whatsaap.link[recomendacao[3]]
-
- 
-
-}
-*/
 
 
 /*contadores*/
@@ -175,13 +212,13 @@ window.document.getElementById('lista2-1').style.display="none"
 
 
 
+   window.document.getElementById('lista0').style.display="none"
 
-function lista0animacao(a) {
+function lista0animacao() {
+ 
     console.log('lista 0')
     if (window.document.getElementById('lista0').style.display=="none") 
     {
-     
-        
         console.log('abriu!')
         /*window.document.getElementById('contlist').style.width="unset"*/
         window.document.getElementById('lista0').style.display="flex"
